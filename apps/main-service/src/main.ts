5bfa0342b4cdcbae6475171074e10e7e230bc1de
LieuNyan <lieu3376@gmail.com>
2025-12-23 11:26:32 +0900

    main-service: create nestjs app template

---
+++ apps/main-service/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import path from 'node:path';
+import { config as dotenvFlowConfig } from 'dotenv-flow';
+
+// Monorepo root directory
+const rootDir = path.resolve(__dirname, '../../..');
+
+// Load environment variables from root directory
+dotenvFlowConfig({
+  path: rootDir,
+});
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT_MAINSERVICE ?? 3000);
+}
+bootstrap();
